2fed08607870280d6c18b0d7d2a0bff41104437a
Carlos Ibanez <carlos.ibanez@live.se>
2025-03-20 15:24:56 +0100

    Created simple site

---
+++ auktionapp/src/pages/createauction.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const  Createauction= () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Välkommen till min auktionssidan!</h1>
+      </header>
+      <main>
+        <p>Detta är en enkel hemsida skapad med React och JSX.</p>
+      </main>
+      
+    </div>
+  );
+}
+
+export default Createauction;
