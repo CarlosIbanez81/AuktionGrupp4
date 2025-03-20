d6f9dabac4de49f4dda4269a76471852c13809b5
Malin Bagewitz Fungdal <malinfungdal@hotmail.com>
2025-03-20 14:59:04 +0100

    skapat middleware för att jämföra sluttid

--- backend/src/models/newAuctionSchema.js
+++ backend/src/models/newAuctionSchema.js
@@ -1,12 +1,15 @@
 const mongoose = require ('mongoose')
+const useAuctionMiddleware = require('../middlewars/AuctionMiddleware')
 
 const AuctionSchema = new mongoose.Schema({
-
+    user: { type: String, required: true},
     object: { type: String, required: true },
     text: { type: String, required: true},
     price: { type: Number, required: true },
     duration: { type: Number, enum: [7,14], required: true },
     endTime: { type: Date, required: true}   
-}, { timestamps: true })
+}, { timestamps: true });
+
+useAuctionMiddleware(AuctionSchema);
 
 module.exports = mongoose.model ('Auction', AuctionSchema)
