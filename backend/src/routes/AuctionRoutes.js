ac74f95764b0ebfcda2251832e70882ceed00ced
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-27 15:22:40 +0100

    conflikthantering

--- backend/src/routes/AuctionRoutes.js
+++ backend/src/routes/AuctionRoutes.js
@@ -1,15 +1,15 @@
 const { createAuction } = require('../controllers/auktionController');
+const express = require('express');
 
-
-const express = require("express");
 const router = express.Router();
-const deleteAuction = require('../controllers/malinsAuctionController')
+
+const {deleteAuction} = require('../controllers/malinsAuctionController')
 
 
 router.post('/', createAuction);
 // router.get('/', getAuctions);
 // router.get('/:id', getAuction);
-router.get('/search', searchAuctions);
+// router.get('/search', searchAuctions);
 router.delete('/:id', deleteAuction);
 
 module.exports = router;
