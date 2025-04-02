8b71ace1f3c7b48b2daa8f0cccf4a396eac9ec22
Sebastiankjernholm <Sebastiankjernholm@hotmail.com>
2025-04-02 16:25:07 +0200

    connected backend first

--- backend/src/models/BidModel.js
+++ backend/src/models/BidModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const bidSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    price: { type: Number, required: true, min: 1 }, 
-    timestamp: { type: Date, default: Date.now } 
+    user: { type: String, required: false },
+    auction: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction', required: true }, 
+    price: { type: Number, required: true, min: 1 },
+    timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Bid', bidSchema);
--- backend/src/models/newAuctionSchema.js
+++ backend/src/models/newAuctionSchema.js
@@ -7,7 +7,8 @@ const AuctionSchema = new mongoose.Schema({
     text: { type: String, required: true},
     price: { type: Number, required: true },
     duration: { type: Number, enum: [7,14], required: true },
-    endTime: { type: Date, required: true}   
+    endTime: { type: Date, required: true},   
+    bids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bid' }] 
 }, { timestamps: true });
 
 useAuctionMiddleware(AuctionSchema);
